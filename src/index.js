200c495054fdc85c05a3a24dbc72bc2d072c7568
Vladimir Bondar <vladi.bondar@gmail.com>
2021-10-06 20:13:19 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'modern-normalize/modern-normalize.css';
+
+import './js/components/theme-switch';
+
+import './sass/main.scss';
+import './images/sprite.svg';
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    // open: true,
+    compress: true,
+    port: 7777,
+  },
+};
